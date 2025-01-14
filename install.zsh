fe2ee7ce442aff5da6d5fa20e7835f6cc63627c2
cybergene <cybergene@protonmail.com>
2025-01-15 00:21:36 +0900

    Updates Homebrew installation and package management
    Improves Homebrew installation process by updating and upgrading Homebrew before installing packages. This ensures the latest versions of packages are installed.

--- install.zsh
+++ install.zsh
@@ -4,15 +4,16 @@
 if ! command -v brew &>/dev/null; then
   echo "Installing Homebrew..."
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+  # Update and upgrade Homebrew
+  brew update
+  brew upgrade
+  # Install Homebrew packages
+  brew bundle --global
 else
   echo "Homebrew is already installed."
 fi
 
-# Update and upgrade Homebrew
-brew update
-brew upgrade
-# Install Homebrew packages
-brew bundle --global
 
 # Install zplug
 if [[ -z "$(typeset -f zplug)" && ! -d "$HOME/.zplug" ]]; then
