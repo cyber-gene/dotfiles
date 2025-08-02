41820bb4b4b24e04df1db9bfdec136b3fa1e383b
cybergene <cybergene@protonmail.com>
2025-08-03 02:13:12 +0900

    Configures Wezterm terminal
    Initializes the Wezterm configuration file with preferred settings.
    
    Sets font, color scheme, tab behavior, and window size.
    Customizes tab bar colors for active and inactive states.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -10,14 +10,30 @@ config.font = wezterm.font 'UDEV Gothic 35NFLG'
 config.harfbuzz_features = { 'liga=1' }
 
 -- color scheme
-config.color_scheme = 'Molokai (Gogh)'
+config.color_scheme = 'Monokai Pro (Gogh)'
 
--- tab
+-- tab behavior settings
 config.use_fancy_tab_bar = false
 
 -- window size
-config.initial_cols = 120
-config.initial_rows = 30
+config.initial_cols = 80
+config.initial_rows = 25
+config.line_height = 1.1
+
+config.colors = {
+  tab_bar = {
+    background = 'rgb(0, 0, 0)',
+
+    active_tab = {
+      bg_color = 'rgb(54, 53, 55)',
+      fg_color = 'rgb(253, 249, 243)',
+    },
+    inactive_tab = {
+      bg_color = 'rgb(0, 0, 0)',
+      fg_color = 'rgb(253, 249, 243)',
+    },
+  },
+}
 
 -- Finally, return the configuration to wezterm:
 return config
