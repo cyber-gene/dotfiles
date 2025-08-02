1f865e120c6e86a6bc2cce4bce0633017960d329
cybergene <cybergene@protonmail.com>
2025-08-02 23:06:53 +0900

    Adds wezterm configuration file
    Adds a wezterm configuration file with customized settings.
    
    The configuration defines font, color scheme, and initial window size for a personalized terminal experience.

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,23 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- font
+config.font_size = 14
+config.font = wezterm.font 'UDEV Gothic 35NFLG'
+config.harfbuzz_features = { 'liga=1' }
+
+-- color scheme
+config.color_scheme = 'Molokai (Gogh)'
+
+-- tab
+use_fancy_tab_bar = true
+
+--
+config.initial_cols = 120
+config.initial_rows = 30
+
+-- Finally, return the configuration to wezterm:
+return config
